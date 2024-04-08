cb9bba90959e5f8f59625501a746a0f4a8c94b1a
bamartinezd <alejandromardelg@gmail.com>
2024-04-08 18:03:16 -0500

    feat(even-odd-pairs): Added new algorithm validating even odd pairs.

---
+++ even-odd-pairs/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program { 
+    public static void Main(String[] args)
+    {
+        Console.WriteLine(HasEvenOddPairs(new int[] { 1,2,3,4,5,7 }));
+        Console.ReadLine();
+    }
+
+    public static bool HasEvenOddPairs(int[] nums) {
+        
+        if (nums.Count() % 2 == 0)
+        {
+            int numOfEvens = 0;
+
+            foreach (var even in nums)
+            {
+
+                if (even % 2 == 0)
+                {
+                    numOfEvens++;    
+                }
+
+            }
+
+            if (numOfEvens == nums.Count()/2)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
