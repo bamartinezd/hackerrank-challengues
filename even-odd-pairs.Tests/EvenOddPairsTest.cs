5022850e772e519bc9745f917ee232ff4d68b13f
bamartinezd <alejandromardelg@gmail.com>
2024-04-13 16:29:41 -0500

    test(even-odd-pairs): Add full test coverage to the algorithm.

--- even-odd-pairs.Tests/EvenOddPairsTest.cs
+++ even-odd-pairs.Tests/EvenOddPairsTest.cs
@@ -4,9 +4,41 @@ namespace even_odd_pairs.Tests
     public class EvenOddPairsTest
     {
         [Test]
-        public Task HasEvenOddPairsTest()
+        public void WhenExecuteHasEvenOddPairsIfNumberOfElementsIsOddReturnFalse()
         {
+            //Arrange
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
             
+            //Act
+            var result = EvenOddPairs.HasEvenOddPairs(numbers);
+            
+            //Assert
+            Assert.False(result);
+        }
+
+        [Test]
+        public void WhenExecuteHasEvenOddPairsIfEvenAndOddArePairsReturnTrue() {
+            //Arrange
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            //Act
+            var result = EvenOddPairs.HasEvenOddPairs(numbers);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Test]
+        public void WhenExecuteHasEvenOddPairsIfEvenAndOddAreNotPairsReturnFalse()
+        {
+            //Arrange
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 7 };
+
+            //Act
+            var result = EvenOddPairs.HasEvenOddPairs(numbers);
+
+            //Assert
+            Assert.False(result);
         }
     }
 }
