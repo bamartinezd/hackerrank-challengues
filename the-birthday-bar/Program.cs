c9f1e47a03ca958519929e63951b17750b76ff8e
bamartinezd <alejandromardelg@gmail.com>
2024-07-21 15:04:59 -0500

    feat(the-birthday-bar) Implementing algorithm from HackerRank.

---
+++ the-birthday-bar/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args){
+
+        // https://www.hackerrank.com/challenges/the-birthday-bar/
+        Console.WriteLine(birthday(new List<int>{2,2,1,3,2}, 4, 2));
+    }
+
+    public static int birthday(List<int> s, int d, int m)
+    {
+        int counter = 0;
+
+        Queue<int> s_queue = new Queue<int>(s);
+        while(s_queue.Count > 0){
+            var resSum = s_queue.Take(m).Sum();
+            
+            if(resSum == d){
+                counter++;
+            }
+
+            s_queue.Dequeue();
+        }
+
+        return counter;
+    }
+}
