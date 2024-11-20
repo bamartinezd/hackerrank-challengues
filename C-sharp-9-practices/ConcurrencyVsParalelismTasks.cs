8440fc8d8c8f5d409db58d7ffbdcaa760d9c5057
alejandromartinezdelgado <alejandromardelg@gmail.com>
2024-11-20 10:29:24 -0500

    feat(gap): add a new project

---
+++ C-sharp-9-practices/ConcurrencyVsParalelismTasks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C_sharp_9_practices
+{
+    public static class ConcurrencyVsParalelismTasks
+    {
+        public static async Task<string> GetFirstTaskAsync(){
+            Console.WriteLine($"Starting {nameof(GetFirstTaskAsync)}");
+            Thread.Sleep(5000);
+            Console.WriteLine($"{nameof(GetFirstTaskAsync)} Completed");
+            return "uno";
+        }
+
+        public static async Task GetSecondTaskAsync(){
+            Console.WriteLine($"Starting {nameof(GetSecondTaskAsync)}");
+            Thread.Sleep(5000);
+            Console.WriteLine($"{nameof(GetSecondTaskAsync)} Completed");
+        }
+
+        public static async Task GetThirdTaskAsync(){
+            Console.WriteLine($"Starting {nameof(GetThirdTaskAsync)}");
+            Thread.Sleep(5000);
+            Console.WriteLine($"{nameof(GetThirdTaskAsync)} Completed");
+        }
+    }
+
+}
