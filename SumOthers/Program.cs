833aa0259b31e059f20e13ee89b30159a96b1e52
bamartinezd <alejandromardelg@gmail.com>
2024-12-26 22:36:28 -0500

    (SumOthers): Implementing new HR algorithm

---
+++ SumOthers/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var arr = new int[] { 1, 2, 3, 4, 5, 6 };
+
+        var resSum = arr.Sum();
+        int sum = 0;
+
+        foreach (int i in arr) {
+            sum = sum + i;
+        }
+          
+        foreach (int i in arr) { 
+            sum = resSum - i;
+            Console.WriteLine(sum);
+        }
+    }
+}
